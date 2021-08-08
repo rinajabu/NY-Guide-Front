f738e2198a74377e6af129f7a0d3e710edd03417
Tommy Chung <tommychung@TC-MacBook-Pro.local>
2021-08-07 21:26:29 -0400

    Created multiple modals

--- src/App.js
+++ src/App.js
@@ -21,7 +21,14 @@ const [newLikes, setNewLikes] = useState(0)
 const [newComment, setNewComment] = useState([])
 const [recommend, setRecommend] = useState([])
 // modal state
-const [open, setOpen] = useState(false)
+const [signUp, setSignUp] = useState(false)
+const [login, setLogin] = useState(false)
+const [newCreate, setNewCreate] = useState(false)
+const [newEdit, setNewEdit] = useState(false)
+// signUp and Login
+const [newUser, setNewUser] = useState('')
+const [newPass, setNewPass] = useState('')
+const [userList, setUserList] = useState([])
 
 ////////////////// EVENT HANDLERS ///////////////////
 
@@ -140,7 +147,7 @@ const handleEditForm = (eventEdit) => {
                 description: newDescription || eventEdit.description,
                 price: newPrice || eventEdit.price,
                 rating: newRating || eventEdit.rating,
-                comments: newComment || eventEdit.comments
+                comments: newComment
             }
         ).then(() => {
             axios
@@ -164,10 +171,49 @@ const handleDelete = (eventDelete) => {
         })
 }
 
-// open and close modal functions
-const onOpenModal = () => {setOpen(true)}
-const onCloseModal = () => {setOpen(false)}
+// Sign up Form
+const signUpForm = (event) => {
+    axios.post(
+        'https://ny-guide-backend-rina-tommy.herokuapp.com/users',
+        {
+            username: newUser,
+            password: newPass
+        }
+    ).then(() => {
+        axios
+            .get('https://ny-guide-backend-rina-tommy.herokuapp.com/users')
+            .then((response) => {
+                setUserList(response.data)
+            })
+    })
+}
 
+//Login form
+const loginForm = (event) => {
+    axios.post(
+        'https://ny-guide-backend-rina-tommy.herokuapp.com/sessions',
+        {
+            username: newUser,
+            password: newPass
+        }
+    ).then(() => {
+        axios
+            .get('https://ny-guide-backend-rina-tommy.herokuapp.com/sessions')
+            .then((response) => {
+                setUserList(response.data)
+            })
+    })
+}
+
+// open and close modal functions
+const signUpOpenModal = () => {setSignUp(true)}
+const signUpCloseModal = () => {setSignUp(false)}
+const loginOpenModal = () => {setLogin(true)}
+const loginCloseModal = () => {setLogin(false)}
+const createOpenModal = () => {setNewCreate(true)}
+const createCloseModal = () => {setNewCreate(false)}
+const editOpenModal = () => {setNewEdit(true)}
+const editCloseModal = () => {setNewEdit(false)}
 //================useEffect================//
 useEffect(() => {
     axios
@@ -182,53 +228,74 @@ useEffect(() => {
   return (
     <main>
       <header>
-        <button onClick={onOpenModal}>Open Modal</button>
-        <Modal open={open} onClose={onCloseModal} center>
-            <h3>Hello this is our modal!</h3>
+        <button onClick={signUpOpenModal}>Sign Up</button>
+        <Modal open={signUp} onClose={signUpCloseModal} center>
+            <h3>Sign Up</h3>
+                <form onSubmit={signUpForm}>
+                    <label for="username">Username: </label>
+                    <input type="text" onChange={newUser} /><br/>
+                    <label for="password">Password: </label>
+                    <input type="password" onChange={newPass} /><br/>
+                     <input type="submit" value="Sign Up!" />
+                </form>
+        </Modal>
+        <button onClick={loginOpenModal}>Login</button>
+        <Modal open={login} onClose={loginCloseModal} center>
+            <h3>Login</h3>
+                <form onSubmit={loginForm}>
+                    <label for="username">Username: </label>
+                    <input type="text" onChange={newUser} /><br/>
+                    <label for="password">Password: </label>
+                    <input type="password" onChange={newPass} /><br/>
+                     <input type="submit" value="Login" />
+                </form>
         </Modal>
       </header>
       <h1>NY Guide</h1>
       <section>
-        <h2>Create NY Recommendation</h2>
-        <form onSubmit={handleNewFormSubmit}>
-          <label for="title">Title </label>
-          <input type="text" onChange={handleNewTitleChange} /><br/>
-          <label for="author">Author </label>
-          <input type="text" onChange={handleNewAuthorChange} /><br/>
-          <label for="category">Category </label>
-          <select onChange={handleNewCategoryChange} defaultValue={newCategory} >
-            <option value="outdoor">Outdoor</option>
-            <option value="food">Food</option>
-            <option value="museum">Museum</option>
-            <option value="sight-seeing">Sight Seeing</option>
-            <option value="night-life">Night Life</option>
-          </select><br/>
-          <label for="location">Location </label>
-          <input type="text" onChange={handleNewLocationChange} /><br/>
-          <label for="image-preview">Image Preview </label>
-          <img src={newImage} />
-          <label for="image">Image URL </label>
-          <input type="url" onChange={handleNewImageChange} /><br/>
-          <label for="description">Description </label>
-          <input type="text" onChange={handleNewDescriptionChange} /><br/>
-          <label for="price">Price </label>
-          <select onChange={handleNewPriceChange} defaultValue={newPrice} >
-            <option value="$">$</option>
-            <option value="$$">$$</option>
-            <option value="$$$">$$$</option>
-            <option value="$$$$">$$$$</option>
-            <option value="$$$$$">$$$$$</option>
-          </select><br/>
-          <label for="rating">Rating </label>
-          <select onChange={handleNewRatingChange} defaultValue={newRating} >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select><br/>
-          <input type="submit" value="Create Recommendation!" />
-        </form>
+        <button onClick={createOpenModal}>Create Recommendation</button>
+        <Modal open={newCreate} onClose={createCloseModal} center>
+            <h2>Create NY Recommendation</h2>
+                <form onSubmit={handleNewFormSubmit}>
+                <label for="title">Title </label>
+                <input type="text" onChange={handleNewTitleChange} /><br/>
+                <label for="author">Author </label>
+                <input type="text" onChange={handleNewAuthorChange} /><br/>
+                <label for="category">Category </label>
+                <select onChange={handleNewCategoryChange} defaultValue={newCategory} >
+                    <option value="outdoor">Outdoor</option>
+                    <option value="food">Food</option>
+                    <option value="museum">Museum</option>
+                    <option value="sight-seeing">Sight Seeing</option>
+                    <option value="night-life">Night Life</option>
+                </select><br/>
+                <label for="location">Location </label>
+                <input type="text" onChange={handleNewLocationChange} /><br/>
+                <label for="image-preview">Image Preview </label>
+                <img src={newImage} />
+                <label for="image">Image URL </label>
+                <input type="url" onChange={handleNewImageChange} /><br/>
+                <label for="description">Description </label>
+                <input type="text" onChange={handleNewDescriptionChange} /><br/>
+                <label for="price">Price </label>
+                <select onChange={handleNewPriceChange} defaultValue={newPrice} >
+                    <option value="$">$</option>
+                    <option value="$$">$$</option>
+                    <option value="$$$">$$$</option>
+                    <option value="$$$$">$$$$</option>
+                    <option value="$$$$$">$$$$$</option>
+                </select><br/>
+                <label for="rating">Rating </label>
+                <select onChange={handleNewRatingChange} defaultValue={newRating} >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select><br/>
+                <input type="submit" value="Create Recommendation!" />
+            </form>
+        </Modal>
       </section>
       <section>
         <h3>NY Recommendations</h3>
@@ -250,7 +317,9 @@ useEffect(() => {
                             <input type="submit" value="Comment"/>
                           </form>
                         </details>
-                        <details><summary>Edit</summary>
+                        <button onClick={editOpenModal}>Edit</button>
+                        <Modal open={newEdit} onClose={editCloseModal} center>
+                            <h3>Edit Recommendation</h3>
                             <form onSubmit={ (event) => {handleEditForm(guide)} }>
                                 <label for="title">Title </label>
                                 <input type="text" onChange={handleNewTitleChange} defaultValue={guide.title} /><br/>
@@ -288,7 +357,7 @@ useEffect(() => {
                                 </select><br/>
                                 <input type="submit" value="Edit" />
                             </form>
-                        </details>
+                            </Modal>
                         <button onClick={ (event) => {handleDelete(guide)} }>Delete</button>
                     </div>
                     )
